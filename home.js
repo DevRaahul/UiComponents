f44e048aeff3e39438d467c04e76e4d0547f78de
Rahul <DevRaahul>
2022-03-05 22:05:41 +0530

    added scroll to top functionality

--- home.js
+++ home.js
@@ -1,8 +1,32 @@
 const burgerIcon = document.getElementById("hamburger");
 const sideNav = document.querySelector("nav");
-
+const scrollTop = document.getElementById("scrollTop");
+const rootElement = document.documentElement;
+console.log("JS called");
 burgerIcon?.addEventListener("click", () => {
-  console.log("click");
   sideNav?.classList.toggle("open");
   burgerIcon?.classList.toggle("open");
 });
+
+const detectScroll = () => {
+  let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if (rootElement.scrollTop / scrollTotal > 0.1) {
+    // Show button
+    scrollTop.classList.remove("hide");
+    scrollTop.classList.add("show");
+  } else {
+    // Hide button
+    scrollTop.classList.add("hide");
+    scrollTop.classList.remove("show");
+  }
+};
+
+scrollTop.addEventListener("click", () => {
+  console.log("click");
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+document.addEventListener("scroll", detectScroll);
